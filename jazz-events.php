dab61fcf598127d01deaa1b5985e2d82b852f615
piedracita <arnaldpedros@gmail.com>
2023-11-19 07:38:40 +0100

    update navigation & footer

--- jazz-events.php
+++ jazz-events.php
@@ -7,14 +7,14 @@
     <!-- COMMON-HEAD END -->
 
     <!-- CANONICAL -->
-    <link rel="canonical" href="https://www.jazzeventual.com/jazz-corporativo.php">
+    <link rel="canonical" href="https://www.jazzeventual.com/jazz-events.php">
 
     <!-- COMMON-LOCALIZATION -->
     <?php include "common-php/localization/jazz-corporate.html"; ?>
     <!-- COMMON-LOCALIZATION END -->
 
     <!-- SITE INFORMATION -->
-    <title>Jazz Eventos de Empresa | Grupo de Jazz para evnetos corporativos</title>
+    <title>Jazz Eventos de Empresa | Grupo de Jazz para eventos corporativos</title>
     <meta name="description" content="Grupo de Jazz para eventos corporativos, música en directo para eventos de empresa. Jazz Eventual en formato Solista, Duo o Trío.">
     <meta name="keywords" content="jazz, eventos, corporativo, empresa, música, solista, duo, trio">
 
@@ -39,9 +39,9 @@
     <!-- PAGE HEADER -->
     <div class="page-header">
         <div class="container text-center">
-            <h1 class="page-title">Jazz para Empresas</h1>
+            <h1 class="page-title">Jazz para eventos</h1>
             <ol class="breadcrumb">
-                <li>Grupo de jazz para eventos de empresa. Música en directo para eventos corporativos.</li>
+                <li>Grupo de jazz para eventosprivados y eventos de empresa. Música en directo para eventos corporativos.</li>
             </ol>
         </div>
     </div>
@@ -50,7 +50,7 @@
     <div class="main-wrap">
 
         <!-- COMMON WEDDINGS -->
-        <?php include "common-php/corporate/es.html"; ?>
+        <?php include "common-php/events/es.html"; ?>
         <!-- COMMON WEDDINGS END -->
 
         <!-- DIVIDER -->
@@ -62,7 +62,7 @@
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2 col-xs-12">
                         <div class="text-center">
-                            <h3 class="section-title" id="ceremony">Evento Cocktail</h3>
+                            <h3 class="section-title" id="events-cocktail">Música para Eventos Cocktail</h3>
                             <p>Crea el <strong>evento de empresa con música en directo</strong>, somo un <strong>grupo de jazz</strong> que nos adaptamos a la necesidad del cliente y del evento, siendo la opción cocktail nuestra favorita.</p>
 
                             <p>Podemos crear desde un ambiente más discrteo con <strong>músico solista</strong> (a la guitarra o saxofón) hasta <strong>duos</strong> y <strong>tríos de jazz</strong> que serán la opción perfecta.</p>
@@ -86,7 +86,7 @@
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2 col-xs-12">
                         <div class="text-center">
-                            <h3 class="section-title" id="cocktail">Grupo de Jazz para Amenización</h3>
+                            <h3 class="section-title" id="events-ambience">Jazz de ambiente y amenización</h3>
                             <p>SI buscas llenar el ambiente con <strong>la mejor música en directo</strong>, contáctanos para asegurarte nuestra disponibilidad y precio.</p>
 
                             <p>En una <strong>amenización</strong> con música en directo podemos adaptar la formación y el repertorio, en función del gusto del cliente.</p>
@@ -126,7 +126,7 @@
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2 col-xs-12">
                         <div class="text-center">
-                            <h3 class="section-title" id="dinner">Música para cena de empresa</h3>
+                            <h3 class="section-title" id="events-dinner">La Música perfecta para cenas</h3>
                             <p>Una <strong>cena de empresa con música en directo</strong> es un acierto seguro para realzar un evento de empresa. Consúltanos para conocer disponibilidad y presupuesto.</p>
 
                             <p>Jazz Eventual se adapta a vuestra cena y evento corporativo, pudiéndo ofrecer desde formatos <strong>solistas</strong> como sería <strong>guitarrista de jazz</strong> o <strong>saxofonista</strong>, hasta duos y tríos de música instrumental y jazz.</p>
